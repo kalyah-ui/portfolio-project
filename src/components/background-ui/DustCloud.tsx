25241ff8cb0c635cb82574a7c1c2332d904486cb
kalyah-ui <kalyah.mckesey@gmail.com>
2026-08-18 21:43:09 -0400

    remove horizontal scroll on mobile

--- src/components/background-ui/DustCloud.tsx
+++ src/components/background-ui/DustCloud.tsx
@@ -2,10 +2,10 @@ import './Background.css'
 
 export function NebulaClouds() {
   return (
-    <>
+    <div className='nebula-wrapper'>
       <div className="nebula nebula-1" />
       <div className="nebula nebula-2" />
       <div className="nebula nebula-3" />
-    </>
+    </div>
   );
 }
